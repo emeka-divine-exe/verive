666c51a85571768ad2da58b376793f1ae28767d7
Emeka Divine <emekadivine.exe@gmail.com>
2026-05-22 09:36:27 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -5,9 +5,4 @@ const withPWA = require('next-pwa')({
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 })
-
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = withPWA(nextConfig)
+module.exports = withPWA({ reactStrictMode: true })
